8bc8f0fe272ee450b5153c85393188244095c940
Animesh <animeshsharma2004@gmail.com>
2026-01-28 09:38:12 +0530

    Add files via upload

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,10 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
     isVerified: { type: Boolean, default: false },
-    otp: { type: String }, // store current OTP (hashed or plain for simple use)
-    otpExpires: { type: Date },
   },
   { timestamps: true }
 );
